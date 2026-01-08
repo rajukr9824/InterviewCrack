42a2de46a40bddd26becec5fd0090440bd7373b2
rajukr9824 <rajuk.ug22.cs@nitp.ac.in.com>
2026-01-08 22:54:22 +0530

    ready to deploy

--- server/index.js
+++ server/index.js
@@ -15,9 +15,20 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+
+
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
 
 
 app.use("/api/auth", authRoutes);
@@ -28,7 +39,10 @@ app.use("/api/coding", codingRoutes);
 app.use("/api/user", require("./routes/userRoutes"));
 
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
